66bb3e332b4060720d707a2bd9305a6cf0cfecbd
Matthew CntKillMe <willyoumeryme@gmail.com>
2018-06-28 20:29:49 +0000

    less verbosity when installing

--- install.lua
+++ install.lua
@@ -4,9 +4,9 @@ local fs = require("filesystem")
 
 -- download libraries
 fs.makeDirectory("/home/lib/")
-shell.execute("wget https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/lib/drive.lua /home/lib/drive.lua")
-shell.execute("wget https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/lib/partman.lua /home/lib/partman.lua")
+shell.execute("wget -fq https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/lib/drive.lua /home/lib/drive.lua")
+shell.execute("wget -fq https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/lib/partman.lua /home/lib/partman.lua")
 
 -- download binaries
 fs.makeDirectory("/home/bin/")
-shell.execute("wget https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/bin/parted.lua /home/bin/parted.lua")
+shell.execute("wget -fq https://raw.githubusercontent.com/cntkillme/OC-Drive-Tools/v2/bin/parted.lua /home/bin/parted.lua")
